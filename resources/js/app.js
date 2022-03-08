ecfacde3e9d97e3c2336e49d0082bf47ad7c3e35
Enzo-Radel <enzo@radel.com.br>
2022-03-08 15:19:45 -0300

    criação dos componentes vue de layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,9 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', require('./components/layout/Header.vue').default);
+Vue.component('main-component', require('./components/layout/Main.vue').default);
+Vue.component('footer-component', require('./components/layout/Footer.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
