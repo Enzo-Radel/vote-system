60ee29cb3e4c49c50e867e8a91ea1d3a9198a359
Enzo-Radel <enzo@radel.com.br>
2022-03-09 13:28:26 -0300

    uso de um componente formulário em Create

--- resources/js/components/pages/Create.vue
+++ resources/js/components/pages/Create.vue
@@ -4,20 +4,10 @@
             <h2>Criar Enquete</h2>
         </div>
         <div class="card-body">
-            <form :action="rota" method="post" id="enquete-form">
-                <input type="hidden" name="_token" :value="csrf_token">
-                <div class="form-group" id="group-title">
-                    <label for="input-title" class="form-label">Título</label>
-                    <input type="text" name="title" id="input-title" class="form-control">
-                </div>
-                <div class="form-group" id="group-dates">
-                    <label for="input-begin" class="form-label">Início</label>
-                    <input type="date" name="begin" id="input-begin" class="form-control">
-
-                    <label for="input-end" class="form-label">Fim</label>
-                    <input type="date" name="end" id="input-end" class="form-control">
-                </div>
-            </form>
+            <form-component
+                :rota="rota"
+                :csrf_token="csrf_token"
+            />
         </div>
         <div class="card-footer">
             <button class="btn btn-success" @click="criarEnquete">Cadastrar</button>
@@ -27,7 +17,13 @@
 </template>
 
 <script>
+import FormComponent from './utilities/Form.vue'
+
 export default {
+    components: {
+        'form-component': FormComponent
+    },
+
     props: {
         rota: String,
         csrf_token: String,
