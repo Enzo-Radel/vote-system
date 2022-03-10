25b2ecded26aa026039cda2be8ecc33a7197af74
Enzo-Radel <enzo@radel.com.br>
2022-03-10 02:04:29 -0300

    migration inserindo votos na tabela respostas

---
+++ database/migrations/2022_03_10_050118_update_respostas_table_insert_column_votes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateRespostasTableInsertColumnVotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('respostas', function (Blueprint $table) {
+            $table->integer('votes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropColumns('respostas', 'votes');
+    }
+}
