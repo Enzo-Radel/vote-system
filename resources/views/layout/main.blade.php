e36a6a734f78d61e58574ab2946e9769aee8bd7d
Enzo-Radel <enzo@radel.com.br>
2022-03-10 01:32:13 -0300

    remoção de arquivo bootstrap desnecessário

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,6 @@
     <title>Vote System</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/styles.css')}}">
-    {{-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> --}}
 </head>
 <body>
     <div id="app" class="d-flex flex-column justify-content-between h-100 w-100"> 
